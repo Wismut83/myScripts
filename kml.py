33b3efcb7e7509a50db50cb65761c796667a30ff
Wismut83 <43611490+Wismut83@users.noreply.github.com>
2019-09-06 12:17:02 +0500

    az updates

--- kml.py
+++ kml.py
@@ -23,12 +23,14 @@ class kml:
 		z = round(z/len(list_input),accu)
 		return x,y,z
 
-	def middle_lenght(self, list_input):
-		lenght = 0
+	def middle(self, list_input):
+		length, az = 0, 0
 		for list in list_input:
-			lenght += list[3]
-		lenght = round(lenght/len(list_input),3)
-		return lenght
+			length += list[3]
+			az += list[4]
+		length = round(length/len(list_input),3)
+		az = round(az/len(list_input),3)
+		return length, az
 
 	def area_az_dip(self, list):
 		a, b = [], []
@@ -50,13 +52,32 @@ class kml:
 		accu = 3
 		return round(az,1), round(dip,1), round(area,accu)
 
-	def lenght_from_coords(self, list):
+	def az_from_coords(self, list):
+		x1, x2 = list[0][0], list[1][0]
+		y1, y2 = list[0][1], list[1][1]
+
+		dx = x1-x2
+		dy = y1-y2
+		atg = math.degrees(math.atan((y2-y1)/(x2-x1)))
+		if dx<0 and dy<0:
+			az = 90-atg
+		elif dx<0 and dy>0:
+			az = 90+atg
+		elif dx>0 and dy>0:
+			az = 270-atg-180
+		elif dx>0 and dy<0:
+			az = 270+atg-180
+		else:
+			print('no one line')
+		return az
+
+	def length_from_coords(self, list):
 		x = (list[0][0]-list[1][0])*(list[0][0]-list[1][0])
 		y = (list[0][1]-list[1][1])*(list[0][1]-list[1][1])
 		z = (list[0][2]-list[1][2])*(list[0][2]-list[1][2])
 		return math.sqrt(x+y+z)
 
-	def kml_to_list_orientation(self, file, listout):
+	def kml_to_list_orientation(self, file, list_out):
 		with open(file) as kml:
 			data = kml.readlines()
 			for idy, line in enumerate(data):
@@ -80,11 +101,11 @@ class kml:
 							coords[idx] = self.code(coords[idx])
 						x, y, z = self.center(coords)
 						az, dip, area = self.area_az_dip(coords)
-						listout.append([x,y,z,az,dip, shale, area])
+						list_out.append([x,y,z,az,dip, shale, area])
 					except:
 						print('Probably an empty line: {} ({})'.format(idy, file))
 
-	def kml_to_list_size(self, file, listout):
+	def kml_to_list_size(self, file, list_out):
 		dictionary = {}
 		with open(file) as kml:
 			data = kml.readlines()
@@ -109,55 +130,49 @@ class kml:
 							coords[idx] = self.code(coords[idx])
 
 						x, y, z = self.center(coords)
-
-						lenght = self.lenght_from_coords(coords)
-
+						length = self.length_from_coords(coords)
+						az = self.az_from_coords(coords)
 						if dictionary.get(name,0) == 0:
 							dictionary.update({name:[]})
-						dictionary[name].append([x, y, z, lenght])
+						dictionary[name].append([x, y, z, length, az])
 					except:
 						print('Probably an empty line: {} ({})'.format(idy, file))
 
 		for name in dictionary:
+			# print(name, dictionary[name])
 			x, y, z = self.center(dictionary[name])
-			lenght = self.middle_lenght(dictionary[name])
-			listout.append([x,y,z,lenght])
+			length, az = self.middle(dictionary[name])
+			list_out.append([x,y,z,length, az])
 
-
-
-
-
-
-
-
-	def list_to_txt(self, listinput, waytxt):
+	def list_to_txt(self, list_input, waytxt):
 		coords = open(waytxt, 'a')
 
 		with open(waytxt, "a") as file:
-			for line in listinput:
+			for line in list_input:
 				a = [str(i) for i in line]
 				file.write(' '.join(a) + '\n')
 
-	def kml_to_txt(self, filename=r'\resutByPy.txt'):
+	def kml_to_txt_triangle(self, filename=r'\resut_triangle.txt'):
 		waytxt = self.way + filename
 		with open(waytxt, "w") as file:
 			file.write('X Y Z Az Dip Shale Square\n')
 		os.chdir(self.way)
 		for file in glob.glob("*.kml"):
-			listout = []
-			self.kml_to_list_orientation(file,listout)
-			self.list_to_txt(listout, waytxt)
+			list_out = []
+			self.kml_to_list_orientation(file,list_out)
+			self.list_to_txt(list_out, waytxt)
 
-	def kml_to_txt_line(self, filename=r'\resutByPy.txt'):
+	def kml_to_txt_line(self, filename=r'\resut_length.txt'):
 		waytxt = self.way + filename
 		with open(waytxt, "w") as file:
-			file.write('X Y Z lenght\n')
+			file.write('X Y Z length az\n')
 		os.chdir(self.way)
 		for file in glob.glob("*.kml"):
-			listout = []
-			self.kml_to_list_size(file, listout)
-			self.list_to_txt(listout, waytxt)
+			list_out = []
+			self.kml_to_list_size(file, list_out)
+			self.list_to_txt(list_out, waytxt)
 
 # a = kml(r"Z:\работы\2019\Олкон\рабочие файлы\2 этап\поиск трещин\Q3")
+# a.kml_to_txt_triangle()
 a = kml(r'D:\ivan')
 a.kml_to_txt_line()
